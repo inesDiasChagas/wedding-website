13618597b2ffee0f29e85250fd4b2a3cc6a2f482
inesDiasChagas <inesdiaschagas@gmail.com>
2025-04-09 19:16:40 +0100

    SchedulePage style ajustement

--- src/pages/SchedulePage.js
+++ src/pages/SchedulePage.js
@@ -1,71 +1,73 @@
 import React, { useState } from "react";
 import SectionWrapper from "../components/Sections/SectionWrapper";
+import cocktail from "../styles/images/cocktail.png";
+
 import "./SchedulePage.css";
 
 const scheduleItems = [
   {
     time: "16:00 – 16:30",
     title: "Chegada dos Convidados",
-    description: "",
-    icon: "🪑",
-    sectionColour: "#8F674D",
-    titleColour: "#2E1F14",
-    descriptionColour: "#4E392A",
+    description: "Bla Bla Bla",
+    icon: cocktail,
+    sectionColour: "#3F4E48",
+    titleColour: "#262F2B",
+    descriptionColour: "#262F2B",
   },
   {
     time: "16:30 – 17:00",
     title: "Cerimónia",
-    description: "",
-    icon: "💍",
-    sectionColour: "#B9977B",
-    titleColour: "#5A3F2D",
-    descriptionColour: "#7C5E48",
+    description: "Bla bla bla Bla",
+    icon: cocktail,
+    sectionColour: "#6E5F58",
+    titleColour: "#423935",
+    descriptionColour: "#423935",
   },
   {
     time: "17:00 – 18:30",
     title: "Cocktail & Fotos",
-    description: "",
-    icon: "🥂",
-    sectionColour: "#ECD8C0",
-    titleColour: "#7F6A53",
-    descriptionColour: "#90816B",
+    description: "Bla bla bla Bla",
+    icon: cocktail,
+    sectionColour: "#A49382",
+    titleColour: "#62584E",
+    descriptionColour: "#62584E",
     button: true,
   },
   {
     time: "18:30 – 19:00",
     title: "Sentar nos Lugares",
     description: "Consulta a disposição dos lugares aqui.",
-    icon: "🪑",
-    sectionColour: "#F4D5A1",
-    titleColour: "#8A784E",
-    descriptionColour: "#A08E64",
+    icon: cocktail,
+    sectionColour: "#D9CBB9",
+    titleColour: "#827A6F",
+    descriptionColour: "#827A6F",
   },
   {
     time: "19:00 – 21:30",
     title: "Jantar + Dedicatorias",
-    description: "",
-    icon: "🍽️",
-    sectionColour: "#F1F5E0",
-    titleColour: "#7C816A",
-    descriptionColour: "#98A08D",
+    description: "Bla Bla Bla",
+    icon: cocktail,
+    sectionColour: "#8A7566",
+    titleColour: "#53463D",
+    descriptionColour: "#53463D",
   },
   {
     time: "21:30 – 22:00",
     title: "Bolo + Champanhe",
-    description: "",
-    icon: "🍰",
-    sectionColour: "#CBDAAB",
-    titleColour: "#5F6A3E",
-    descriptionColour: "#6E7C4A",
+    description: "Bla Bla Bla",
+    icon: cocktail,
+    sectionColour: "#BFAE9A",
+    titleColour: "#73685C",
+    descriptionColour: "#73685C",
   },
   {
     time: "22:00 – 01:00",
     title: "Dança + Open Bar",
-    description: "",
-    icon: "💃",
-    sectionColour: "#B5C2A6",
-    titleColour: "#4E563E",
-    descriptionColour: "#6E7A5C",
+    description: "Bla Bla Bla",
+    icon: cocktail,
+    sectionColour: "#5F5C5A",
+    titleColour: "#393736",
+    descriptionColour: "#393736",
   },
 ];
 
@@ -93,14 +95,11 @@ const SchedulePage = () => {
             backgroundColor: previousItem.sectionColour,
           }}
         >
-          <div
+          <img
             className='icon-overlay-inner'
-            style={{
-              backgroundColor: currentItem.sectionColour,
-            }}
-          >
-            <span style={{ fontSize: "1.5rem" }}>{currentItem.icon}</span>
-          </div>
+            src={currentItem.icon}
+            alt={currentItem.title}
+          ></img>
         </div>
 
         {scheduleItems.map((item, index) => {
